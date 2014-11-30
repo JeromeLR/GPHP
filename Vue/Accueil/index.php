3dec41e35daf4963fce7dd8913c28a56c6f8c857
JeromeLR <jerome@bourdais.info>
2014-11-30 18:01:27 +0100

    mineur

--- Vue/Accueil/index.php
+++ Vue/Accueil/index.php
@@ -1,17 +1,16 @@
 <?php $this->titre = "Mon Blog"; ?>
 
-<?php foreach ($billets as $billet):
-    ?>
-<a href="<?= "billet/index/" . $this->nettoyer($billet['id']) ?>">
-    <article  class="drop-shadow curved curved-hz-2">
-        <header>
-            
-                <h1 class="titreBillet"><?= $this->nettoyer($billet['titre']) ?></h1>
-            <time><?= $this->nettoyer($billet['date']) ?></time>
-        </header>
-        <p><?= $this->nettoyer($billet['contenu']) ?></p>
-    </article>
-</a>
+<div class="col-md-8 col-md-offset-2">
+    <?php foreach ($billets as $billet): ?>
+        <a href="<?= "billet/index/" . $this->nettoyer($billet['id']) ?>">
+            <article  class="drop-shadow curved curved-hz-2">
+                <header>
 
-    
-<?php endforeach; ?>
+                        <h1 class="titreBillet"><?= $this->nettoyer($billet['titre']) ?></h1>
+                    <time><?= $this->nettoyer($billet['date']) ?></time>
+                </header>
+                <p><?= $this->nettoyer($billet['contenu']) ?></p>
+            </article>
+        </a>
+    <?php endforeach; ?>
+</div>
